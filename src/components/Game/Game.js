a73ade5133bb95c51334ff9b1015e7ff76144bab
Micael Persson <micael.c.persson@gmail.com>
2019-01-11 11:56:58 +0100

    got basic gameplay working.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,17 +5,53 @@ import './Game.css';
 
 class Game extends Component {
   state = {
+    games: [],
     houses: Array(100).fill(false),
-    rounds: 0,
+    nrOfZombies: 1,
+    days: 0,
   }
 
-  resetGame() {
+  reset() {
     const houses = Array(100).fill(false);
 
     this.setState({
       houses,
-      rounds: 0,
+      days: 0,
+      nrOfZombies: 1
     });
+  }
+
+  newRound() {
+    const games = [...this.state.games];
+    games.push(this.state.days);
+
+    this.setState({games});
+    this.reset()
+  }
+
+  sendZombie(houses, index) {
+    if (houses[index] === true) {
+      return null;
+    } else {
+      houses[index] = true;
+      return 1;
+    }
+  }
+
+  playDay() {
+    const houses = [...this.state.houses];
+    let newZombies = 0;
+
+    for (let i = 0; i < this.state.nrOfZombies; i++) {
+      const houseTarget = Math.floor(Math.random() * houses.length);
+      newZombies += this.sendZombie(houses, houseTarget);
+    }
+
+    this.setState({
+      houses,
+      nrOfZombies: this.state.nrOfZombies + newZombies,
+      days: this.state.days + 1,
+    })
   }
 
   renderHouses() {
@@ -26,8 +62,21 @@ class Game extends Component {
   }
 
   render() {
+    let gameInfo = <button onClick={() => this.playDay()}>New day!</button>
+
+    if (this.state.nrOfZombies > 100) {
+      gameInfo = <button onClick={() => this.newRound()}>New round!</button>
+    }
+
+    if (this.state.games.length === 10) {
+      gameInfo = <div>
+        Over 10 rounds it took an average of x days to infect all houses!
+      </div>
+    }
+    
     return (
       <div className="Game">
+      {gameInfo}
         <div>
           {this.renderHouses()}
         </div>
